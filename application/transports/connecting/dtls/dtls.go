c58df61b806bae42b9ec8aee2053eba697dd55e0
Mingye Chen <mingyechen82@gmail.com>
2022-12-24 17:53:35 -0500

    Use dtls pkg

--- application/transports/connecting/dtls/dtls.go
+++ application/transports/connecting/dtls/dtls.go
@@ -2,25 +2,17 @@ package dtls
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
 	"net"
-	"sync"
-	"time"
 
-	"github.com/pion/dtls/v2"
-	"github.com/pion/dtls/v2/pkg/crypto/selfsign"
 	dd "github.com/refraction-networking/conjure/application/lib"
 	"github.com/refraction-networking/conjure/application/transports"
+	"github.com/refraction-networking/conjure/pkg/dtls"
 	pb "github.com/refraction-networking/gotapdance/protobuf"
 )
 
-const testPort int = 4443
-
 type Transport struct {
-	dtlsListener net.Listener
-	connMap      map[string]chan net.Conn
-	mapMutex     sync.RWMutex
+	dtlsListener *dtls.Listener
 }
 
 // Name returns name of the transport
@@ -40,53 +32,16 @@ func (t Transport) GetIdentifier(reg *dd.DecoyRegistration) string {
 
 // NewDTLSTransport creates a new dtls transport
 func NewDTLSTransport() (*Transport, error) {
-	udpAddr := &net.UDPAddr{IP: nil, Port: testPort}
+	addr := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 4444}
 
-	certificate, err := selfsign.GenerateSelfSigned()
-
+	listener, err := dtls.Listen(addr)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create udp conn: %w", err)
-	}
-	// Create parent context to cleanup handshaking connections on exit.
-	ctx, _ := context.WithCancel(context.Background())
-
-	// Prepare the configuration of the DTLS connection
-	config := &dtls.Config{
-		Certificates:         []tls.Certificate{certificate},
-		ExtendedMasterSecret: dtls.RequireExtendedMasterSecret,
-		// Create timeout context for accepted connection.
-		ConnectContextMaker: func() (context.Context, func()) {
-			return context.WithTimeout(ctx, 30*time.Second)
-		},
-	}
-
-	listener, err := dtls.Listen("udp", udpAddr, config)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to create udp conn: %w", err)
+		return nil, fmt.Errorf("error creating dtls listner: %v", err)
 	}
 
 	return &Transport{
 		dtlsListener: listener,
-		connMap:      make(map[string]chan net.Conn),
-		mapMutex:     sync.RWMutex{},
 	}, nil
-}
-
-func (t *Transport) acceptLoop() {
-	for {
-		newConn, err := t.dtlsListener.Accept()
-		if err != nil {
-			continue
-		}
-
-		go func() {
-			t.mapMutex.lock()
-			if t.connMap[newConn.RemoteAddr().String()] == nil {
-				t.connMap[newConn.RemoteAddr().String()] = make(chan net.Conn)
-			}
-		}()
-	}
 }
 
 // Connect takes a registraion and returns a dtls Conn connected to the client
@@ -95,5 +50,10 @@ func (t *Transport) Connect(ctx context.Context, reg *dd.DecoyRegistration) (net
 		return nil, transports.ErrNotTransport
 	}
 
-	return nil, fmt.Errorf("not implemented")
+	conn, err := t.dtlsListener.AcceptFromSecret(reg.Keys.SharedSecret)
+	if err != nil {
+		return nil, fmt.Errorf("error accepting dtls from secret: %v", err)
+	}
+
+	return conn, nil
 }
