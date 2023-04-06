de985a60c0984df4309ec3e1b0fb9b93fc3a9a59
Mingye Chen <mingyechen82@gmail.com>
2023-04-06 17:08:32 -0400

    Add port randomization

--- application/transports/connecting/dtls/dtls.go
+++ application/transports/connecting/dtls/dtls.go
@@ -9,33 +9,41 @@ import (
 	"github.com/refraction-networking/conjure/application/transports"
 	"github.com/refraction-networking/conjure/pkg/dtls"
 	pb "github.com/refraction-networking/gotapdance/protobuf"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
-const port int = 41245
+const (
+	// port range boundaries for min when randomizing
+	portRangeMin = 1024
+	portRangeMax = 65535
+	defaultPort  = 443
+)
+
+const listenPort = 41245
 
 type Transport struct {
 	dtlsListener *dtls.Listener
 }
 
 // Name returns name of the transport
-func (t Transport) Name() string {
+func (Transport) Name() string {
 	return "dtls"
 }
 
 // LogPrefix returns log prefix of the transport
-func (t Transport) LogPrefix() string {
+func (Transport) LogPrefix() string {
 	return "DTLS"
 }
 
 // GetIdentifier returns an identifier unique a registration
-func (t Transport) GetIdentifier(reg *dd.DecoyRegistration) string {
+func (Transport) GetIdentifier(reg *dd.DecoyRegistration) string {
 	return string(reg.Keys.ConjureHMAC("dtlsTrasportHMACString"))
 }
 
 // NewTransport creates a new dtls transport
 func NewTransport() (*Transport, error) {
-	addr := &net.UDPAddr{Port: port}
+	addr := &net.UDPAddr{Port: listenPort}
 
 	listener, err := dtls.Listen(addr)
 	if err != nil {
@@ -61,8 +69,17 @@ func (t *Transport) Connect(ctx context.Context, reg *dd.DecoyRegistration) (net
 	return conn, nil
 }
 
-func (t Transport) GetDstPort(libVersion uint, seed []byte, params any) (uint16, error) {
-	return uint16(port), nil
+func (Transport) GetDstPort(libVersion uint, seed []byte, params any) (uint16, error) {
+	parameters, ok := params.(*pb.GenericTransportParams)
+	if !ok {
+		return 0, fmt.Errorf("bad parameters provided")
+	}
+
+	if parameters.GetRandomizeDstPort() {
+		return transports.PortSelectorRange(portRangeMin, portRangeMax, seed)
+	}
+
+	return defaultPort, nil
 }
 
 func (Transport) GetProto() pb.IPProto {
@@ -70,5 +87,7 @@ func (Transport) GetProto() pb.IPProto {
 }
 
 func (Transport) ParseParams(libVersion uint, data *anypb.Any) (any, error) {
-	return nil, nil
+	var m = &pb.GenericTransportParams{}
+	err := anypb.UnmarshalTo(data, m, proto.UnmarshalOptions{})
+	return m, err
 }
