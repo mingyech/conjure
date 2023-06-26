fe039749d4bbeae605fbaa245a4d6d193eb396b0
Mingye Chen <mingyechen82@gmail.com>
2023-06-26 13:29:41 +0400

    More tests

--- pkg/heartbeat/heartbeat_test.go
+++ pkg/heartbeat/heartbeat_test.go
@@ -1,9 +1,7 @@
 package heartbeat
 
 import (
-	"fmt"
 	"net"
-	"sync"
 	"testing"
 	"time"
 
@@ -21,18 +19,13 @@ func TestReadWrite(t *testing.T) {
 	err = Client(client, conf)
 	require.Nil(t, err)
 
-	var wg sync.WaitGroup
-	wg.Add(2)
-
 	sent := 0
 	recvd := 0
 	toSend := []byte("testtt")
+	stop := time.After(conf.Interval * 2)
 
 	go func() {
-		defer wg.Done()
-		stop := time.Now().Add(conf.Interval*2 + 10*time.Millisecond)
-		fmt.Printf("Now: %v, Stop: %v\n", time.Now(), stop)
-		for !time.Now().After(stop) {
+		for {
 			buffer := make([]byte, 4096)
 			n, err := s.Read(buffer)
 			if err != nil {
@@ -44,18 +37,79 @@ func TestReadWrite(t *testing.T) {
 	}()
 
 	go func() {
-		defer wg.Done()
-		stop := time.Now().Add(conf.Interval * 2)
-		for !time.Now().After(stop) {
-			_, err := client.Write(toSend)
-			require.Nil(t, err)
-			sent++
-			time.Sleep(10 * time.Millisecond)
+		for {
+			select {
+			case <-stop:
+				return
+			default:
+				_, err := client.Write(toSend)
+				require.Nil(t, err)
+				sent++
+				time.Sleep(10 * time.Millisecond)
+			}
 		}
 	}()
 
-	wg.Wait()
-	fmt.Printf("sent: %v, recvd: %v", sent, recvd)
+	<-stop
 
 	require.Equal(t, sent, recvd)
+}
+
+func TestSend(t *testing.T) {
+	server, client := net.Pipe()
+
+	readCh := make(chan []byte)
+
+	go func() {
+		for {
+			buffer := make([]byte, 4096)
+			n, err := server.Read(buffer)
+			if err != nil {
+				continue
+			}
+
+			readCh <- buffer[:n]
+		}
+	}()
+
+	err := Client(client, conf)
+	require.Nil(t, err)
+
+	duration := 2
+	stop := time.After(conf.Interval*time.Duration(duration) + 10*time.Millisecond)
+
+	hbCount := 0
+	for {
+		select {
+		case b := <-readCh:
+			require.Equal(t, conf.Heartbeat, b)
+			hbCount++
+		case <-stop:
+			require.Equal(t, duration+1, hbCount)
+			return
+		}
+	}
+
+}
+
+func TestTimeout(t *testing.T) {
+	server, client := net.Pipe()
+	go func() {
+		for {
+			buffer := make([]byte, 4096)
+			client.Read(buffer)
+		}
+	}()
+
+	s, err := Server(server, conf)
+	require.Nil(t, err)
+
+	_, err = s.Write([]byte("123"))
+	require.Nil(t, err)
+
+	stop := time.After(conf.Interval + 10*time.Millisecond)
+	<-stop
+	_, err = s.Write([]byte("123"))
+	require.NotNil(t, err)
+
 }
